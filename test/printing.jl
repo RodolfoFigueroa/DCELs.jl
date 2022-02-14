485efc732b5403592d99e645fa2d87a482f831d9
Rodolfo Figueroa Soriano <4rodolfofigueroa2@gmail.com>
2022-02-14 12:01:08 -0600

    Fix printing tests

--- test/printing.jl
+++ test/printing.jl
@@ -3,25 +3,26 @@
     vr = repr(v)
     @test occursin("ID: $(v.id)\n", vr)
     @test occursin("Coords: ($(v.x), $(v.y))\n", vr)
-    @test occursin("Edge: $(v.edge)\n", vr)
+
+    @test occursin("Edge: $(v.edge === nothing ? "nothing" : v.edge.id)\n", vr)
 end
 
 @testset "Edge" begin
     e = Edge()
     er = repr(e)
     @test occursin("ID: $(e.id)\n", er)
-    @test occursin("Next: $(e.next)\n", er)
-    @test occursin("Prev: $(e.prev)\n", er)
-    @test occursin("Twin: $(e.twin)\n", er)
-    @test occursin("Dest: $(e.dest)\n", er)
-    @test occursin("Face: $(e.face)\n", er)
+    @test occursin("Next: $(e.next === nothing ? "nothing" : e.next.id)\n", er)
+    @test occursin("Prev: $(e.prev === nothing ? "nothing" : e.prev.id)\n", er)
+    @test occursin("Twin: $(e.twin.id)\n", er)
+    @test occursin("Dest: $(e.dest.id)\n", er)
+    @test occursin("Face: $(e.face === nothing ? "nothing" : e.face.id)\n", er)
 end
 
 @testset "Face" begin
     f = Face()
     fr = repr(f)
     @test occursin("ID: $(f.id)\n", fr)
-    @test occursin("Edge: $(f.edge)\n", fr)
+    @test occursin("Edge: $(f.edge === nothing ? "nothing" : f.edge.id)\n", fr)
 end
 
 @testset "Tessellation" begin
